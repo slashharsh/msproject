fedd2ad8f2f4f26133f64db7134b927e852401e5
Harsh Mangal <harsh.mangal1108@gmail.com>
2024-01-02 19:42:37 +0530

    product service implementation

---
+++ product-service/src/main/java/com/msproject/productservice/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.msproject.productservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.msproject.productservice.dto.ProductRequest;
+import com.msproject.productservice.dto.ProductResponse;
+import com.msproject.productservice.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/product")
+public class ProductController {
+	
+	private final ProductService productService;
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createProduct(@RequestBody ProductRequest productRequest) {
+		productService.createProduct(productRequest);
+	}
+	
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public List<ProductResponse> getAllProducts() {
+		return productService.getAllProducts();
+	}
+
+}
---
+++ product-service/src/main/java/com/msproject/productservice/service/ProductService.java
@@ -0,0 +1,50 @@
+package com.msproject.productservice.service;
+
+import com.msproject.productservice.dto.ProductRequest;
+import com.msproject.productservice.dto.ProductResponse;
+//import com.msproject.productservice.dto.ProductResponse;
+import com.msproject.productservice.model.Product;
+import com.msproject.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+	
+	private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+    Product product = Product.builder()
+    		.name(productRequest.getName())
+    		.description(productRequest.getDescription())
+    		.price(productRequest.getPrice())
+    		.build();
+    
+    productRepository.save(product);
+    log.info("Product Is Saved: ", product.getId());
+    
+    
+    
+    }
+
+	public List<ProductResponse> getAllProducts() {
+		List<Product> products = productRepository.findAll();
+		return products.stream().map(this::mapToProductResponse).toList();
+
+		
+	}
+
+	private ProductResponse mapToProductResponse(Product product) {
+		return ProductResponse.builder()
+				.id(product.getId())
+				.name(product.getName())
+				.description(product.getDescription())
+				.price(product.getPrice())
+				.build();
+	}
+}
