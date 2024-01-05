fe8e22dc96c56ca3af494859205cfb6c9a016b02
Harsh Mangal <harsh.mangal1108@gmail.com>
2024-01-05 23:19:49 +0530

    added order service

---
+++ order-service/src/main/java/com/msproject/orderservice/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.msproject.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
---
+++ order-service/src/main/java/com/msproject/orderservice/service/OrderService.java
@@ -0,0 +1,44 @@
+package com.msproject.orderservice.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.msproject.orderservice.dto.OrderLineItemsDto;
+import com.msproject.orderservice.dto.OrderRequest;
+import com.msproject.orderservice.model.Order;
+import com.msproject.orderservice.model.OrderLineItems;
+import com.msproject.orderservice.repository.OrderRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+	
+	private final OrderRepository orderRepository;
+
+	public void placeOrder(OrderRequest orderRequest) {
+		Order order = new Order();
+		order.setOrderNumber(UUID.randomUUID().toString());
+		
+		List<OrderLineItems> orderLineItems =  orderRequest.getOrderLineItemsDtoList()
+		.stream()
+		.map(orderLineItemsDto -> mapToDto(orderLineItemsDto))
+		.toList();
+		
+		order.setOrderLineItemsList(orderLineItems);
+		orderRepository.save(order);
+		
+	}
+
+	private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+		OrderLineItems orderLineItems = new OrderLineItems();
+		orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+		orderLineItems.setPrice(orderLineItemsDto.getPrice());
+		orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+		return orderLineItems;
+	}
+	
+}
